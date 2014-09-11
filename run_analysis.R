604cfc72a101826a9f827b0f8b92d44b1d143958
Sergei Kirsanov <skirsanov@yandex-team.ru>
2014-09-11 23:40:25 +0400

    load test and train data

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,9 @@
 #!/usr/bin/env Rscript
 
+root_data_folder <- "UCI HAR Dataset"
+
 # Download dataset
-if (!file.exists("UCI HAR Dataset")){
+if (!file.exists(root_data_folder)){
     dataset_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
     temp <- tempfile()
     download.file(dataset_url, destfile = temp, method="curl", mode="wb")
@@ -9,8 +11,38 @@ if (!file.exists("UCI HAR Dataset")){
     unlink(temp)
 }
 
-#Read raw data
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names =  c("id", "label"))
-features <- read.table("UCI HAR Dataset/features.txt", col.names = c("id", "name"))
+# Read raw data
+# common
+activity_labels <- read.table(paste0(root_data_folder, "/activity_labels.txt"), col.names =  c("activity.id", "activity.label"))
+features <- read.table(paste0(root_data_folder, "/features.txt"), col.names = c("id", "name"))
 
+readset <- function(setname) {
+    set_folder <- paste0(root_data_folder,"/", setname)    
+    x_path <- paste0(set_folder, "/X_", setname, ".txt")
+    y_path <- paste0(set_folder, "/y_", setname, ".txt") 
+    subject_path <- paste0(set_folder, "/subject_", setname, ".txt")
+    
+    message(paste("reading data from:", x_path))    
+    x <- read.table(x_path, col.names = features$name)
+    
+    message(paste("reading data from:", y_path))
+    y <- read.table(y_path, col.names = "activity.id")
+    
+    message(paste("reading data from:", subject_path))
+    subjects <- read.table(subject_path, col.names = "subject.id")
+    
+    xy <- cbind(x, y, subjects)
+    
+    message(paste(setname, "data loaded"))
+    xy
+}
+
+train_data <- readset("train")
+test_data <- readset("test")
+
+all_data <- rbind(train_data, test_data)
+data <- merge(all_data, activity_labels, by="activity.id")
+
+
+#x_test <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$name)
 
